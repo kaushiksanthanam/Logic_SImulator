2579f9e37342275142ef039099ddea06c03ae7b8
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-10-31 07:01:27 -0400

    designer commit

---
+++ Logic Simulator/Logic Simulator/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Logic_Simulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.fileDisplayTextBox = new System.Windows.Forms.TextBox();
+            this.parseFileButton = new System.Windows.Forms.Button();
+            this.outputFormulaTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputStringTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.applyInputButton = new System.Windows.Forms.Button();
+            this.outputValueTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(12, 12);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.ReadOnly = true;
+            this.filePathTextBox.Size = new System.Drawing.Size(272, 20);
+            this.filePathTextBox.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(290, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(35, 20);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // fileDisplayTextBox
+            // 
+            this.fileDisplayTextBox.Location = new System.Drawing.Point(12, 67);
+            this.fileDisplayTextBox.Multiline = true;
+            this.fileDisplayTextBox.Name = "fileDisplayTextBox";
+            this.fileDisplayTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.fileDisplayTextBox.Size = new System.Drawing.Size(480, 440);
+            this.fileDisplayTextBox.TabIndex = 2;
+            // 
+            // parseFileButton
+            // 
+            this.parseFileButton.Location = new System.Drawing.Point(332, 12);
+            this.parseFileButton.Name = "parseFileButton";
+            this.parseFileButton.Size = new System.Drawing.Size(119, 20);
+            this.parseFileButton.TabIndex = 3;
+            this.parseFileButton.Text = "Parse File";
+            this.parseFileButton.UseVisualStyleBackColor = true;
+            this.parseFileButton.Click += new System.EventHandler(this.parseFileButton_Click);
+            // 
+            // outputFormulaTextBox
+            // 
+            this.outputFormulaTextBox.Location = new System.Drawing.Point(498, 67);
+            this.outputFormulaTextBox.Multiline = true;
+            this.outputFormulaTextBox.Name = "outputFormulaTextBox";
+            this.outputFormulaTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputFormulaTextBox.Size = new System.Drawing.Size(657, 217);
+            this.outputFormulaTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "INPUT FILE CONTENTS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OUTPUT FORMULA";
+            // 
+            // inputStringTextBox
+            // 
+            this.inputStringTextBox.Location = new System.Drawing.Point(603, 12);
+            this.inputStringTextBox.Name = "inputStringTextBox";
+            this.inputStringTextBox.Size = new System.Drawing.Size(206, 20);
+            this.inputStringTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Input String";
+            // 
+            // applyInputButton
+            // 
+            this.applyInputButton.Location = new System.Drawing.Point(815, 10);
+            this.applyInputButton.Name = "applyInputButton";
+            this.applyInputButton.Size = new System.Drawing.Size(131, 22);
+            this.applyInputButton.TabIndex = 9;
+            this.applyInputButton.Text = "Apply Input";
+            this.applyInputButton.UseVisualStyleBackColor = true;
+            this.applyInputButton.Click += new System.EventHandler(this.applyInputButton_Click);
+            // 
+            // outputValueTextBox
+            // 
+            this.outputValueTextBox.Location = new System.Drawing.Point(498, 310);
+            this.outputValueTextBox.Multiline = true;
+            this.outputValueTextBox.Name = "outputValueTextBox";
+            this.outputValueTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputValueTextBox.Size = new System.Drawing.Size(657, 197);
+            this.outputValueTextBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(498, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "OUTPUT VALUES";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1167, 519);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputValueTextBox);
+            this.Controls.Add(this.applyInputButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputStringTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputFormulaTextBox);
+            this.Controls.Add(this.parseFileButton);
+            this.Controls.Add(this.fileDisplayTextBox);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Name = "MainForm";
+            this.Text = "Logic Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox fileDisplayTextBox;
+        private System.Windows.Forms.Button parseFileButton;
+        private System.Windows.Forms.TextBox outputFormulaTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputStringTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button applyInputButton;
+        private System.Windows.Forms.TextBox outputValueTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Logic Simulator/Logic Simulator/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using Logic_Simulator_BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic_Simulator
+{
+    public partial class MainForm : Form
+    {
+        public List<int> inputs;
+        public List<int> outputs;
+        public List<LogicRecord> logicRecords;
+        public List<string> outputFormula;
+
+        Dictionary<int, int> inputSignalsDict;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetMembers();
+        }
+
+        private void SetMembers()
+        {
+            inputs = new List<int>();
+            outputs = new List<int>();
+            logicRecords = new List<LogicRecord>();
+            outputFormula = new List<string>();
+
+            outputFormulaTextBox.Clear();
+            inputStringTextBox.Clear();
+            inputSignalsDict = new Dictionary<int, int>();
+            outputValueTextBox.Clear();
+
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            string fileName = ofd.FileName;
+            filePathTextBox.Text = fileName;
+        }
+
+        private void parseFileButton_Click(object sender, EventArgs e)
+        {
+            SetMembers();
+
+            fileDisplayTextBox.Clear();
+            outputFormulaTextBox.Clear();
+
+            FileReader fileReader = new FileReader();
+            logicRecords = fileReader.GetLogicRecords(filePathTextBox.Text);
+            fileDisplayTextBox.Text = File.ReadAllText(filePathTextBox.Text);
+
+            inputs = fileReader.GetInputs(filePathTextBox.Text);
+            outputs = fileReader.GetOutputs(filePathTextBox.Text);
+
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            foreach (int output in outputs)
+            {
+                ConstructFormula(output);
+            }
+        }
+
+        private void ConstructFormula(int output)
+        {
+            LogicRecord logicRecord = logicRecords.Find(obj => obj._output == output);
+            string formula;
+            if (logicRecord._operator == Operators.BUF || logicRecord._operator == Operators.INV)
+            {
+                formula = "(" + logicRecord._operator + " " + logicRecord._inputs.ElementAt(0) + ")";
+            }
+            else
+            {
+                formula = GetSimplest(logicRecord._inputs.ElementAt(0)) + " " + logicRecord._operator.ToString() + " " + GetSimplest(logicRecord._inputs.ElementAt(1));
+            }
+            outputFormula.Add(formula);
+            outputFormulaTextBox.AppendText(output + " = " + formula + "\n\n");
+        }
+
+        private string GetSimplest(int input)
+        {
+            LogicRecord logicRecord = logicRecords.Find(obj => obj._output == input);
+            if (logicRecord == null)
+            {
+                return input.ToString();
+            }
+            else
+            {
+                if (logicRecord._operator == Operators.BUF || logicRecord._operator == Operators.INV)
+                {
+                    string part1 = GetSimplest(logicRecord._inputs.ElementAt(0));
+                    string operatorPart = logicRecord._operator.ToString();
+                    return "(" + operatorPart + " " + part1 + ")";
+                }
+                else
+                {
+                    string part1 = GetSimplest(logicRecord._inputs.ElementAt(0));
+                    string operatorPart = logicRecord._operator.ToString();
+                    string part2 = GetSimplest(logicRecord._inputs.ElementAt(1));
+
+                    return "(" + part1 + " " + operatorPart + " " + part2 + ")";
+                }
+            }
+        }
+
+        private void applyInputButton_Click(object sender, EventArgs e)
+        {
+            outputValueTextBox.Clear();
+
+            inputSignalsDict = new Dictionary<int, int>();
+            string inputString = inputStringTextBox.Text;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                inputSignalsDict.Add(inputs.ElementAt(i), int.Parse(inputString.ElementAt(i).ToString()));
+            }
+
+            foreach (int output in outputs)
+            {
+                outputValueTextBox.AppendText(output + " = " + GetSimplExp(output) + "\n\n");
+            }
+        }
+
+        private int GetSimplExp(int output)
+        {
+            LogicRecord logicRecord = logicRecords.Find(obj => obj._output == output);
+            int outputValue = -1;
+
+            if (logicRecord == null)
+            {
+                bool success = inputSignalsDict.TryGetValue(output, out outputValue);
+                return outputValue;
+            }
+            else
+            {
+                switch (logicRecord._operator)
+                {
+                    case Operators.INV:
+                        return (GetSimplExp(logicRecord._inputs.ElementAt(0)) == 1) ? 0 : 1;
+                    case Operators.BUF:
+                        return (GetSimplExp(logicRecord._inputs.ElementAt(0)));
+
+                    case Operators.AND:
+                        return GetSimplExp(logicRecord._inputs.ElementAt(0)) & GetSimplExp(logicRecord._inputs.ElementAt(1));
+
+                    case Operators.OR:
+                        return GetSimplExp(logicRecord._inputs.ElementAt(0)) | GetSimplExp(logicRecord._inputs.ElementAt(1));
+
+                    case Operators.NAND:
+                        return ((GetSimplExp(logicRecord._inputs.ElementAt(0)) & GetSimplExp(logicRecord._inputs.ElementAt(1))) == 1) ? 0 : 1;
+
+                    case Operators.NOR:
+                        return ((GetSimplExp(logicRecord._inputs.ElementAt(0)) | GetSimplExp(logicRecord._inputs.ElementAt(1))) == 1) ? 0 : 1;
+
+                    default:
+                        return -1;
+                }
+            }
+        }
+    }
+}
